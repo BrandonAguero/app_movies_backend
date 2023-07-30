fb538ead8b8df316527413d1cd3a555e1b9db9e4
Brandon <name.brandon.aguero@gmail.com>
2023-07-30 16:24:41 -0500

    Añadí modelos, controladores y rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,15 @@
-const express = require('express');
+const express = require("express");
+const routerMovies = require("./movies.router.js");
+const routerGenres = require("./genres.router.js");
+const routerActors = require("./actors.router.js");
+const routerDirectors = require("./directors.router.js");
+
 const router = express.Router();
 
 // colocar las rutas aquí
-
-
+router.use("/movies", routerMovies);
+router.use("/genres", routerGenres);
+router.use("/actors", routerActors);
+router.use("/directors", routerDirectors);
 
 module.exports = router;
